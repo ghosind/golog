f959909145a07a33fc22fb74acf3a0ed0513bb42
Chen Su <ghosind@gmail.com>
2022-02-28 12:00:02 +0800

    <test> add test cases for TextFormatter

---
+++ text_formatter_test.go
@@ -0,0 +1,57 @@
+package golog
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func Test_TextFormatterWithDefaultFormat(t *testing.T) {
+	entry := Entry{
+		Level:   InfoLevel,
+		Time:    time.Now(),
+		Message: "Hello World",
+	}
+	formatter := TextFormatter{}
+
+	buf, err := formatter.Format(&entry)
+	if err != nil {
+		t.Errorf("TextFormatter Format() returns %v error, expect no error", err)
+	}
+
+	expect := fmt.Sprintf("[%s] [%s] %s",
+		entry.Time.Format(defaultTimestampFormat),
+		GetLevel(entry.Level),
+		entry.Message,
+	)
+	if string(buf) != expect {
+		t.Errorf("TextFormatter Format() returns \"%s\", expect \"%s\"", string(buf), expect)
+	}
+}
+
+func Test_TextFormatterWithCustomFormat(t *testing.T) {
+	entry := Entry{
+		Level:   InfoLevel,
+		Time:    time.Now(),
+		Message: "Hello World",
+	}
+	timestampFormat := "2006-01-02 15:04:05"
+	formatter := TextFormatter{
+		LogFormat:       "%level - %timestamp - %message",
+		TimestampFormat: timestampFormat,
+	}
+
+	buf, err := formatter.Format(&entry)
+	if err != nil {
+		t.Errorf("TextFormatter Format() returns %v error, expect no error", err)
+	}
+
+	expect := fmt.Sprintf("%s - %s - %s",
+		GetLevel(entry.Level),
+		entry.Time.Format(timestampFormat),
+		entry.Message,
+	)
+	if string(buf) != expect {
+		t.Errorf("TextFormatter Format() returns \"%s\", expect \"%s\"", string(buf), expect)
+	}
+}
