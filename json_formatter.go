c6b4a8c2052d60d5ac1261a11ae9da7e0b6f54e9
Chen Su <ghosind@gmail.com>
2022-03-01 16:26:43 +0800

    <feat> allow custom field name in json formatter

--- json_formatter.go
+++ json_formatter.go
@@ -3,6 +3,9 @@ package golog
 import "encoding/json"
 
 type JSONFormatter struct {
+	// Fields indicates custom fields' name.
+	Fields map[string]string
+	// TimestampFormat is the timestamp format template with time.Format rules.
 	TimestampFormat string
 }
 
@@ -14,9 +17,23 @@ func (formatter JSONFormatter) Format(entry *Entry) ([]byte, error) {
 		timestampFormat = defaultTimestampFormat
 	}
 
-	data["time"] = entry.Time.Format(timestampFormat)
-	data["level"] = entry.Level
-	data["message"] = entry.Message
+	timestampField := KeyTimestamp
+	if name := formatter.Fields[KeyTimestamp]; name != "" {
+		timestampField = name
+	}
+	data[timestampField] = entry.Time.Format(timestampFormat)
+
+	levelField := KeyLevel
+	if name := formatter.Fields[KeyLevel]; name != "" {
+		levelField = name
+	}
+	data[levelField] = GetLevel(entry.Level)
+
+	messageField := KeyMessage
+	if name := formatter.Fields[KeyMessage]; name != "" {
+		messageField = name
+	}
+	data[messageField] = entry.Message
 
 	buf, err := json.Marshal(data)
 	return buf, err
